2a31685de0361174be63fc3c0bbe1926672cd867
Robin Hübner <profan@pagefault.se>
2014-12-30 20:54:48 +0100

    Scaffolding.

--- bk-tree.lua
+++ bk-tree.lua
@@ -1,3 +1,22 @@
 local bk_tree = {}
 
+function bk_tree:new()
+	local n_obj = {}
+	setmetatable(n_obj, self)
+	self.__index = self
+	return n_obj
+end
+
+function bk_tree:insert(val)
+	
+end
+
+function bk_tree:remove()
+	
+end
+
+function bk_tree:query(str)
+	
+end
+
 return bk_tree
