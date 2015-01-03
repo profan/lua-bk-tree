3ca306e3226042aa9c7c7e0c688641d728c54034
Robin Hübner <profan@pagefault.se>
2015-01-03 15:03:36 +0100

    Add small example.

---
+++ example.lua
@@ -0,0 +1,36 @@
+print ("Simple spellcheck example!")
+
+bktree = require "bk-tree"
+
+words = {"book", "bark", "car", "dog", "beauty", "beautiful", "definitely", "immediately", "accidentally"}
+
+tree = bktree:new("book")
+
+print("available words.")
+for i=2, #words do
+	print(words[i])
+	tree:insert(words[i])
+end
+
+io.write("input a word to test: ")
+io.flush()
+word = io.read()
+
+io.write("input edit distance(small numbers): ")
+io.flush()
+distance = io.read()
+
+result = tree:query(word, tonumber(distance))
+
+if result then
+
+	print("candidates: ")
+	for k, v in pairs(result) do
+	    print (v)
+	end
+
+else
+
+	print("no results!")
+
+end
